1b8f3e0bdeedb6ee8d048a9711d1695605e89dcb
CSRocker <clarachenae@gmail.com>
2016-04-16 21:53:19 -0700

    Change App Architecture

--- app/src/main/java/com/example/clarachen/locateipark/HomeScreen.java
+++ app/src/main/java/com/example/clarachen/locateipark/HomeScreen.java
@@ -14,93 +14,19 @@ import android.view.MenuItem;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.clarachen.locateipark.R;
-import com.example.clarachen.locateipark.HomeMap;
-import com.example.clarachen.locateipark.Info;
-import com.example.clarachen.locateipark.Notification;
-
 public class HomeScreen extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        viewPager =(ViewPager) findViewById(R.id.viewpager);
-        setupViewPager(viewPager);
-
-        tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
 
 
     }
 
-    private void setupViewPager(ViewPager viewPager){
-        ViewPagerAdapter adapter= new ViewPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new HomeMap(),"Home");
-        adapter.addFragment(new Info(),"Info");
-        adapter.addFragment(new Notification(),"Notify");
-        viewPager.setAdapter(adapter);
-    }
 
-    class ViewPagerAdapter extends FragmentPagerAdapter{
-        private final List<Fragment> mFragmentList = new ArrayList<>();
-        private final List<String> mFragmentTitleList = new ArrayList<>();
-
-        public ViewPagerAdapter(FragmentManager manager){
-            super(manager);
-        }
-
-        @Override
-        public Fragment getItem(int position){
-            return mFragmentList.get(position);
-        }
-
-        @Override
-        public int getCount(){
-            return mFragmentList.size();
-        }
-
-        public void addFragment(Fragment fragment,String title){
-            mFragmentList.add(fragment);
-            mFragmentTitleList.add(title);
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position){
-            return mFragmentTitleList.get(position);
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
 
 }
